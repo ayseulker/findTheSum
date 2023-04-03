aba5ffa34df44717ce0c831435a3c17e793b1c18
ayseulker <aysenrulkerr@gmail.com>
2023-04-03 15:12:04 +0300

    #

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 
 function findSum (a,b){
     let sum = 0;
+
+    if(a<4 || b>99){
+        alert("Lütfen 4-99 arasında bir değer giriniz")
+
+    }
     for (let number =a; number<=b; number++){
         if(number % 3==0 && number %5 ==0){
             sum += number;
@@ -12,6 +17,6 @@ function findSum (a,b){
 
 }
 
-findSum (0,45)
+findSum (5,100)
 
 
